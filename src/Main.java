8570ce2d29c2fd98c18ddc7a4d97051096f77b5f
Wolvye <alainsoltau@gmail.com>
2024-10-13 13:47:57 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,35 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        String eingabe ="";
-        eingabe = JOptionPane.showInputDialog("Eine Beispiel NAchricht");
-        System.out.println(eingabe);
+        float [] begrenzung = new float [2];
+        begrenzung[0] =0.5f;
+        begrenzung[1] =0.7f;
+        float ergebnis = begrenzteZufallsZahl(begrenzung);
+        System.out.println(ergebnis);
+
+
+    }
+    public static float begrenzteZufallsZahl(float[] begrenzung){
+        //generieren
+        boolean gefunden =true;
+        while (gefunden){
+
+
+            float zufallsZahl = (float) Math.random();
+
+            gefunden =false;
+            // ist enthalten?
+            for (int i = 0; i<begrenzung.length;i++){
+                float schublade = begrenzung[i];
+                // ja-> neue generieren
+                if(zufallsZahl == schublade){
+                    gefunden =true;
+                }
+            } //nein ->zurück geben
+            if (gefunden ==false){
+                return zufallsZahl;
+            }
+        }
+        return  -1;
     }
 }
