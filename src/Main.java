12c6990937a405b016a22201ef786f8ea5abeb6d
Wolvye <alainsoltau@gmail.com>
2024-10-13 13:26:41 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(berechnen(2));
-     }
-    public static int berechnen(int i){
-        if (i ==0){
-            return i;
-        }
-        for (; i<10 ; ){
-            i=i*i;
-        }return i;
+        String eingabe ="";
+        eingabe = JOptionPane.showInputDialog("Eine Beispiel NAchricht");
+        System.out.println(eingabe);
     }
 }
