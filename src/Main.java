ea3adcc3663a7fc982abc3cd5e4cdfecf50dcd96
Wolvye <alainsoltau@gmail.com>
2024-10-13 13:03:56 +0200

    update lessons

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        String[] stringArray = new String[1];
-        stringArray[0] = "Hallo Welt";
-        String ersteSchublade = stringArray[0];
-        System.out.println(ersteSchublade);
-        int[] intArray = new int[3];
-        intArray[0]=0;
-        intArray[1]=1;
-        intArray[2]=2;
-
-        int ersteZahl = intArray[0];
-        int zweiteZahl = intArray[1];
-        int dritteZahl = intArray[2];
-
-        int zahlenGes = ersteZahl + zweiteZahl + dritteZahl;
-        System.out.println(zahlenGes);
-
+        System.out.println(berechnen(2));
+     }
+    public static int berechnen(int i){
+        if (i ==0){
+            return i;
+        }
+        for (; i<10 ; ){
+            i=i*i;
+        }return i;
     }
 }
